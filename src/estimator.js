ca0b687ae71a739ef6c1ebef57cf78c42ac6658f
n-pretorious <pretoriousndungu@gmail.com>
2020-04-09 15:22:43 +0300

    challenge 1  - calculated impact  - calculated severeImpact

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    const {timeToElapse, reportedCases} = input;
+
+    const totalDays = timeToElapse;
+    const totalWeeks = Math.trunc(totalDays/7); 
+    const totalMonths = Math.trunc(totalDays/30);
+    const currentlyInfected = reportedCases * 10;
+    const severeImpact = reportedCases * 50;
+
+    let infectionsByRequestedTime;
+
+    return {
+        data,
+        impact: {
+            days: infectionsByRequestedTime = currentlyInfected * Math.pow(2, Math.trunc(totalDays/3)),
+            weeks: infectionsByRequestedTime = currentlyInfected * (Math.pow(2, Math.trunc(7/3)) * totalWeeks),
+            months: infectionsByRequestedTime = currentlyInfected * (Math.pow(2, 10) * totalMonths)
+        },
+        severeImpact: {
+            days: infectionsByRequestedTime = severeImpact * Math.pow(2, (totalDays/3)),
+            weeks: infectionsByRequestedTime = severeImpact * (Math.pow(2, Math.trunc(7/3)) * totalWeeks),
+            months: infectionsByRequestedTime = severeImpact * (Math.pow(2, 10) * totalMonths)
+        }
+    }
+};
 
 export default covid19ImpactEstimator;
