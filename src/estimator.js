3a358b31518f0e4a182b305c0567a6b345a5613d
n-pretorious <pretoriousndungu@gmail.com>
2020-04-09 15:43:23 +0300

    fixed linting errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,28 +1,26 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    const {timeToElapse, reportedCases} = input;
+  const input = data;
+  const { timeToElapse, reportedCases } = input;
 
-    const totalDays = timeToElapse;
-    const totalWeeks = Math.trunc(totalDays/7); 
-    const totalMonths = Math.trunc(totalDays/30);
-    const currentlyInfected = reportedCases * 10;
-    const severeImpact = reportedCases * 50;
+  const totalDays = timeToElapse;
+  const totalWeeks = Math.trunc(totalDays / 7);
+  const totalMonths = Math.trunc(totalDays / 30);
+  const currentlyInfected = reportedCases * 10;
+  const severeImpact = reportedCases * 50;
 
-    let infectionsByRequestedTime;
-
-    return {
-        data,
-        impact: {
-            days: infectionsByRequestedTime = currentlyInfected * Math.pow(2, Math.trunc(totalDays/3)),
-            weeks: infectionsByRequestedTime = currentlyInfected * (Math.pow(2, Math.trunc(7/3)) * totalWeeks),
-            months: infectionsByRequestedTime = currentlyInfected * (Math.pow(2, 10) * totalMonths)
-        },
-        severeImpact: {
-            days: infectionsByRequestedTime = severeImpact * Math.pow(2, (totalDays/3)),
-            weeks: infectionsByRequestedTime = severeImpact * (Math.pow(2, Math.trunc(7/3)) * totalWeeks),
-            months: infectionsByRequestedTime = severeImpact * (Math.pow(2, 10) * totalMonths)
-        }
+  return {
+    data,
+    impact: {
+      days: currentlyInfected * Math.pow(2, Math.trunc(totalDays / 3)),
+      weeks: currentlyInfected * (Math.pow(2, Math.trunc(7 / 3)) * totalWeeks),
+      months: currentlyInfected * (Math.pow(2, 10) * totalMonths)
+    },
+    severeImpact: {
+      days: severeImpact * Math.pow(2, (totalDays / 3)),
+      weeks: severeImpact * (Math.pow(2, Math.trunc(7 / 3)) * totalWeeks),
+      months: severeImpact * (Math.pow(2, 10) * totalMonths)
     }
+  }
 };
 
 export default covid19ImpactEstimator;
