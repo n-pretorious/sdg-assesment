c3d92ed8dcd3180de21e96d39d58804ff9216b31
n-pretorious <pretoriousndungu@gmail.com>
2020-04-12 18:30:50 +0300

    fixing challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -5,29 +5,27 @@ const covid19ImpactEstimator = (data) => {
   const currentlyInfected = reportedCases * 10;
   const severeImpact = reportedCases * 50;
 
-  let infectionsByRequestedTimeImpact, infectionsByRequestedTimeServerImpact
+  let infectionsByRequestedTimeImpact;
+  let infectionsByRequestedTimeServerImpact;
 
-  if (periodType === "days") {
+  if (periodType === 'days') {
     const totalDays = Math.trunc(timeToElapse / 3);
 
     infectionsByRequestedTimeImpact = currentlyInfected * (2 ** totalDays);
     infectionsByRequestedTimeServerImpact = severeImpact * (2 ** totalDays);
-
-  } else if (periodType === "weeks") {
+  } else if (periodType === 'weeks') {
     const oneWeek = (2 ** Math.trunc(7 / 3));
     const totalWeeks = Math.trunc(timeToElapse / 30);
 
     infectionsByRequestedTimeImpact = currentlyInfected * (oneWeek * totalWeeks);
     infectionsByRequestedTimeServerImpact = severeImpact * (oneWeek * totalWeeks);
-
   } else {
     const oneMonth = (2 ** 10);
     const totalMonths = Math.trunc(timeToElapse / 30);
 
     infectionsByRequestedTimeImpact = currentlyInfected * (oneMonth * totalMonths);
     infectionsByRequestedTimeServerImpact = severeImpact * (oneMonth * totalMonths);
-
-  };
+  }
 
   return {
     data,
