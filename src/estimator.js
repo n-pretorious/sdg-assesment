aec1a3ba98e6cdbb5187faa90799abdc246d4432
n-pretorious <pretoriousndungu@gmail.com>
2020-04-12 16:35:44 +0300

    fixing output for challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -2,23 +2,18 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const { reportedCases, timeToElapse } = input;
 
-  const totalDays = timeToElapse;
-  const totalWeeks = Math.trunc(totalDays / 7);
-  const totalMonths = Math.trunc(totalDays / 30);
   const currentlyInfected = reportedCases * 10;
   const severeImpact = reportedCases * 50;
 
   return {
     data,
     impact: {
-      days: currentlyInfected * (2 ** Math.trunc(totalDays / 3)),
-      weeks: currentlyInfected * ((2 ** Math.trunc(7 / 3)) * totalWeeks),
-      months: currentlyInfected * ((2 ** 10) * totalMonths)
+      currentlyInfected,
+      infectionsByRequestedTime: currentlyInfected * (2 ** Math.trunc(totalDays / 3))
     },
     severeImpact: {
-      days: severeImpact * (2 ** (totalDays / 3)),
-      weeks: severeImpact * ((2 ** Math.trunc(7 / 3)) * totalWeeks),
-      months: severeImpact * ((2 ** 10) * totalMonths)
+      currentlyInfected: severeImpact,
+      infectionsByRequestedTime: severeImpact * (2 ** Math.trunc(totalDays / 3))
     }
   };
 };
